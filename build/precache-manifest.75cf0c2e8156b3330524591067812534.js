fd41539a8b59f59bf2616d2244a6429c4cdf8c13
Luca Iaconelli <lucabelli@msn.com>
2020-10-01 16:49:42 +0200

    docs: updated github pages

---
+++ build/precache-manifest.75cf0c2e8156b3330524591067812534.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4bfd8f80bef8f65582fa913323f205",
+    "url": "/adidas-interview-game/index.html"
+  },
+  {
+    "revision": "f419e812a1820b143650",
+    "url": "/adidas-interview-game/static/css/2.14376d69.chunk.css"
+  },
+  {
+    "revision": "47478dcbc9b89943a47d",
+    "url": "/adidas-interview-game/static/css/4.5376377d.chunk.css"
+  },
+  {
+    "revision": "62ced10a58d4876c6a91",
+    "url": "/adidas-interview-game/static/css/5.e2d0724d.chunk.css"
+  },
+  {
+    "revision": "efae1d79ef08892b979e",
+    "url": "/adidas-interview-game/static/css/6.45465269.chunk.css"
+  },
+  {
+    "revision": "343bb4c5de1f814448e1",
+    "url": "/adidas-interview-game/static/css/7.cb1f6a08.chunk.css"
+  },
+  {
+    "revision": "440f2c7d2bb7c50b6561",
+    "url": "/adidas-interview-game/static/css/main.f54e800f.chunk.css"
+  },
+  {
+    "revision": "f419e812a1820b143650",
+    "url": "/adidas-interview-game/static/js/2.dd9b1064.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/adidas-interview-game/static/js/2.dd9b1064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3044420471de51c545",
+    "url": "/adidas-interview-game/static/js/3.18fc1b17.chunk.js"
+  },
+  {
+    "revision": "47478dcbc9b89943a47d",
+    "url": "/adidas-interview-game/static/js/4.f13e8959.chunk.js"
+  },
+  {
+    "revision": "62ced10a58d4876c6a91",
+    "url": "/adidas-interview-game/static/js/5.0f15d3a8.chunk.js"
+  },
+  {
+    "revision": "efae1d79ef08892b979e",
+    "url": "/adidas-interview-game/static/js/6.ca51226c.chunk.js"
+  },
+  {
+    "revision": "343bb4c5de1f814448e1",
+    "url": "/adidas-interview-game/static/js/7.abdc6d96.chunk.js"
+  },
+  {
+    "revision": "2ef0ca4d230f142ed31d",
+    "url": "/adidas-interview-game/static/js/8.33c282e7.chunk.js"
+  },
+  {
+    "revision": "440f2c7d2bb7c50b6561",
+    "url": "/adidas-interview-game/static/js/main.1bc906a7.chunk.js"
+  },
+  {
+    "revision": "e935bdd6f368cc7b6c39",
+    "url": "/adidas-interview-game/static/js/runtime-main.4b28caff.js"
+  }
+]);
