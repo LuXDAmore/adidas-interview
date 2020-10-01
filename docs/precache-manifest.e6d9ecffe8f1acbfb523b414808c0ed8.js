4a7b01034fdd6452b7e8bce90f90b002af5c5471
Luca Iaconelli <lucabelli@msn.com>
2020-10-01 17:45:19 +0200

    docs: updated github pages

---
+++ docs/precache-manifest.e6d9ecffe8f1acbfb523b414808c0ed8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddb69363fa4b9bd86336ab8821aa4952",
+    "url": "/adidas-interview/index.html"
+  },
+  {
+    "revision": "c3fd22b76cceb7577c8c",
+    "url": "/adidas-interview/static/css/2.14376d69.chunk.css"
+  },
+  {
+    "revision": "d706a6aacd52c3f4b7b0",
+    "url": "/adidas-interview/static/css/4.5376377d.chunk.css"
+  },
+  {
+    "revision": "459af7af427ecc0ec4a9",
+    "url": "/adidas-interview/static/css/5.e2d0724d.chunk.css"
+  },
+  {
+    "revision": "ab5537d02acac6526c42",
+    "url": "/adidas-interview/static/css/6.45465269.chunk.css"
+  },
+  {
+    "revision": "7ec11ad43e3e4ca240cf",
+    "url": "/adidas-interview/static/css/7.cb1f6a08.chunk.css"
+  },
+  {
+    "revision": "e022dd2682996a3a1043",
+    "url": "/adidas-interview/static/css/main.f54e800f.chunk.css"
+  },
+  {
+    "revision": "c3fd22b76cceb7577c8c",
+    "url": "/adidas-interview/static/js/2.e69ca272.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/adidas-interview/static/js/2.e69ca272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df949baa2a89312e318a",
+    "url": "/adidas-interview/static/js/3.58edc7a2.chunk.js"
+  },
+  {
+    "revision": "d706a6aacd52c3f4b7b0",
+    "url": "/adidas-interview/static/js/4.5a41f363.chunk.js"
+  },
+  {
+    "revision": "459af7af427ecc0ec4a9",
+    "url": "/adidas-interview/static/js/5.f371e6a7.chunk.js"
+  },
+  {
+    "revision": "ab5537d02acac6526c42",
+    "url": "/adidas-interview/static/js/6.030a6186.chunk.js"
+  },
+  {
+    "revision": "7ec11ad43e3e4ca240cf",
+    "url": "/adidas-interview/static/js/7.e95fd7cb.chunk.js"
+  },
+  {
+    "revision": "58362deee752354217e2",
+    "url": "/adidas-interview/static/js/8.ccfb2304.chunk.js"
+  },
+  {
+    "revision": "e022dd2682996a3a1043",
+    "url": "/adidas-interview/static/js/main.c608aa55.chunk.js"
+  },
+  {
+    "revision": "1b5864e3fa9887f8c5f5",
+    "url": "/adidas-interview/static/js/runtime-main.7f43b5d2.js"
+  }
+]);
